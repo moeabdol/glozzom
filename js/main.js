7d68c9f1544543cb0c0a423a85f5e63d23cde7ab
moeabdol <mohd.a.saed@gmail.com>
2017-11-11 21:41:03 +0300

    Implement video section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.video').click(function() {
+    var theModal = $(this).data('target'),
+      videoSrc = $(this).attr('data-video'),
+      videoSrcAuto = videoSrc + '?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1';
+    $(theModal + ' iframe').attr('src', videoSrcAuto);
+    $(theModal + ' button.close').click(function() {
+      $(theModal + ' iframe').attr('src', videoSrc);
+    });
+  });
+});
